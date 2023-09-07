fffac1a26539f143fcfb6f8e491f7ccc6ac505e9
Dan Li <dli1@outlook.com>
2023-09-07 12:50:43 -0600

    Revert "initial commit"
    This reverts commit ce3916b88877e6f151a4ff058f3bcede3fe48bfa.

--- download.py
+++
@@ -1,4 +0,0 @@
-import yfinance
-
-df = yfinance.download('AAPL', start='2023-01-01', end='2023-08-20')
-df.to_csv('AAPL.csv')
--- main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-import sqlite3, config
-
-app = FastAPI()
-@app.get("/")
-def index():
-    return {"hello": "world"}
